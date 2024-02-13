88e105dad3879306f005e2bbc80ab070d444c5fb
dongyeob <maintain98@naver.com>
2024-02-13 13:32:52 +0900

    allBeans

--- src/main/java/kongkong/myrestfulservice/RestfulApiApplication.java
+++ src/main/java/kongkong/myrestfulservice/RestfulApiApplication.java
@@ -2,12 +2,21 @@ package kongkong.myrestfulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestfulApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestfulApiApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(RestfulApiApplication.class, args);
+
+        String[] allBeans = applicationContext.getBeanDefinitionNames();
+
+        for(String beans : allBeans){
+            System.out.println("beans = " + beans);
+        }
+
+
     }
 
 }
